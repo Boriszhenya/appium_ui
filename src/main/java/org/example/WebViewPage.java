bd3e11f97f0ea3efdf9f9b4e7967e8e6739e57f7
Borys Pedorenko <boriszhenya@gmail.com>
2024-03-12 11:32:28 +0100

    - ok

--- src/main/java/org/example/WebViewPage.java
+++ src/main/java/org/example/WebViewPage.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,11 +17,15 @@ import java.util.Set;
 
 public class WebViewPage {
     protected final AndroidDriver driver;
+    
+
+
 
     public WebViewPage(AppiumDriver driver) {
         this.driver = (AndroidDriver) driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
+
 
     public void switchToWebView() {
         Set<String> contextNames = driver.getContextHandles();
@@ -33,9 +42,25 @@ public class WebViewPage {
 
     public String getMainPageText() {
         HomePage homePage = new HomePage(driver);
-        homePage.openMenu("Webview");
+       homePage.openMenu("Webview");
+       //driver.findElement(AppiumBy.xpath("//android.widget.Button[@text='Toggle navigation bar']")).click();
         switchToWebView();
+           //Внутри страницы открыть меню
+        //driver.findElement(By.xpath("//nav/div[1]/div[1]/button/svg/path")).click();
+           //Выбрать пункт “Community”
+       // driver.findElement(By.xpath("//android.view.View[@content-desc='\"'Community']")).click();
+           //Прокрутить вниз страницы
+
+           //Открыть ссылку “Office hours”
+      //  driver.findElement(By.xpath("//android.view.View[@resource-id=\"__docusaurus_skipToContent_fallback\"]/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.widget.TextView")).click();
+
         return driver.findElement(By.cssSelector("header > div > p"))
                 .getText();
     }
+//    public void openMenu1() {
+//        By webButtonLocator = By.xpath("//android.widget.Button[@text=\"Toggle navigation bar\"]");
+//        WebElement menuButton = driver.findElement(webButtonLocator);
+//        menuButton.click();
+//    }
+
 }
--- src/test/java/WebdriverIOAppTest.java
+++ src/test/java/WebdriverIOAppTest.java
@@ -44,13 +44,6 @@ public class WebdriverIOAppTest extends BaseTest {
     @Test
     public void testWebView () {
         final WebViewPage webViewPage = new WebViewPage(driver);
-        webViewPage.getMainPageText();
-        webViewPage.switchToWebView();
-
-
-
-
-
         assertEquals (
                 "Next-gen browser and mobile automation test framework for Node.js",
                 webViewPage.getMainPageText()
